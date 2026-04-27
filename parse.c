75ffc40a9a9596a0343436a71f7d59d11c9b52de
Diego Le Gall <227832533+diego2crocs@users.noreply.github.com>
2026-04-28 00:07:23 +0200

    [Parsing]: Parsing robuste avec overflow et gestion mémorie. - formats '1 2 3' et "1 2 3" supportés - validation entière et détection overflow - doublons refusés - zéros leaks au valgrind

---
+++ parse.c
@@ -0,0 +1,168 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dle-gall <dle-gall@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/27 22:06:25 by dle-gall          #+#    #+#             */
+/*   Updated: 2026/04/27 22:06:25 by dle-gall         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/*
+Verif que la string représente un entier valide
+return 1 si valide, 0 sinon
+*/
+int is_valid_number(char *str)
+{
+	int i;
+
+	i = 0;
+	if (str[0] == '\0')
+		return (0);
+	if (str[0] == '-' || str[0] == '+')
+		i++;
+	if (str[i] == '\0')
+		return (0);
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/*
+	Convertit une string en int	+ gere le INT_MAX et INT_MIN
+*/
+int string_to_int(char *str, int *out)
+{
+	int i;
+	int sign;
+	long result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	if (str[0] == '-' || str[0] == '+')
+	{
+		if (str[0] == '-')
+			sign = sign * -1;
+		i++;
+	}
+	while (str[i])
+	{
+		result = result * 10 + (str[i] - '0');
+		if (result * sign > INT_MAX || result * sign < INT_MIN)
+			return (0);
+		i++;
+	}
+	*out = (int)(result * sign);
+	return (1);
+}
+/*
+	Cherche si 'value' est déjà présent dans la stack
+	return 1 si oui, 0 si non
+*/
+int is_in_stack(t_list *stack, int value)
+{
+	 t_list *p;
+
+	p = stack;
+	while(p)
+	{
+		if (p->data == value)
+			return (1);
+		else
+			p = p->next;
+	}
+	return (0);
+}
+
+/*
+	créer un nouveau noeud t_list avec 'value' et l'ajoute a la fin de stack
+	return 1 si succes, 0 si malloc fail
+*/
+int add_to_stack(t_list **stack, int value)
+{
+	t_list *new_node;
+	t_list *work;
+
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return (0);
+	new_node->data = value;
+	new_node->next = NULL;
+	if (*stack == NULL)
+	{
+		*stack = new_node;
+		return (1);
+	}
+	work = *stack;
+	while (work->next != NULL)
+		work = work->next;
+	work->next = new_node;
+	return (1);
+}
+
+/*
+boucle principale du parsing, pour chaque argv[i], valide et ajoute a la stack
+return 1 si tout est ok, 0 si erreur
+*/
+int parse_args(int argc, char **argv, t_list **stack_a)
+{
+	int i;
+	int j;
+	char **tokens;
+
+	i = 1;
+	while (i < argc)
+	{
+		tokens = ft_split(argv[i], ' ');
+		if (!tokens)
+			return (0);
+		j = 0;
+		while (tokens[j])
+		{
+			if (!process_token(tokens[j], stack_a))
+			{
+				free_split(tokens);
+				return (0);
+			}
+			j++;
+		}
+		free_split(tokens);
+		i++;
+	}
+	return (1);
+}
+
+// MAIN TEMPORAIRE
+
+int main(int argc, char **argv)
+{
+    t_list  *stack_a;
+    t_list  *tmp;
+
+    stack_a = NULL;
+    if (argc < 2)
+        return (0);
+    if (!parse_args(argc, argv, &stack_a))
+    {
+        write(2, "Error\n", 6);
+		free_stack(&stack_a);
+        return (1);
+    }
+    tmp = stack_a;
+    while (tmp)
+    {
+        printf("%d\n", tmp->data);
+        tmp = tmp->next;
+    }
+    free_stack(&stack_a);
+    return (0);
+}
--- push_swap.h
+++ push_swap.h
@@ -13,7 +13,10 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
+# include <limits.h>
 
 typedef struct s_list
 {
@@ -21,6 +24,7 @@ typedef struct s_list
 	struct s_list	*next;
 }	t_list;
 
+// -- ops --
 void	sa(t_list *lst_a, int flag);
 void	sb(t_list *lst_b, int flag);
 void	ss(t_list *lst_a, t_list *lst_b);
@@ -32,5 +36,19 @@ void	rr(t_list **lst_a, t_list **lst_b);
 void	rra(t_list **lst, int flag);
 void	rrb(t_list **lst, int flag);
 void	rrr(t_list **lst_a, t_list **lst_b);
+
+// -- parsing --
+int		is_valid_number(char *str);
+int		string_to_int(char *str, int *out);
+int		add_to_stack(t_list **stack, int value);
+int		parse_args(int argc, char **argv, t_list **stack_a);
+
+// -- utils -- 
+int		ft_isdigit(int c);
+char	**ft_split(char const *s, char c);
+int     process_token(char *token, t_list **stack_a);
+void    free_split(char **tab);
+void    free_stack(t_list **stack);
+int		is_in_stack(t_list *stack, int value);
 
 #endif
